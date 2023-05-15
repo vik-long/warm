3e47aaa213870cea2c22c24666ee412b0905ac93
vik-long <umfrage.viktor@gmx.de>
2023-05-15 15:34:00 +0300

    Fix bug

--- src/feature_20251006.py
+++ src/feature_20251006.py
@@ -24,3 +24,8 @@ def historical_feature():
     """Feature added on 2023-03-10 16:08:00"""
     print('Historical feature working')
     return True
+# Historical update 2023-05-15 15:34:00
+def historical_feature():
+    """Feature added on 2023-05-15 15:34:00"""
+    print('Historical feature working')
+    return True
